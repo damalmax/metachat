abe2c13f80292446fb3c0d63661df1ad96e3cc17
Raman Zakharenka <thehadalone@gmail.com>
2018-09-05 00:34:48 +0300

    Add missing docs

--- metachat/format.go
+++ metachat/format.go
@@ -5,7 +5,7 @@ import (
 	"regexp"
 )
 
-// TODO docs
+// Set of pre-compiled regular expressions for all Metachat tags.
 var (
 	BoldRegexp          = regexp.MustCompile("#{bold}(.*?){bold}#")
 	ItalicRegexp        = regexp.MustCompile("#{italic}(.*?){italic}#")
@@ -15,26 +15,32 @@ var (
 	QuoteRegexp         = regexp.MustCompile("#{quote author=(.*?)}(.*?){quote}#")
 )
 
+// Bold marks text as bold using Metachat tag.
 func Bold(text string) string {
 	return fmt.Sprintf("#{bold}%s{bold}#", text)
 }
 
+// Italic marks text as italic using Metachat tag.
 func Italic(text string) string {
 	return fmt.Sprintf("#{italic}%s{italic}#", text)
 }
 
+// Strikethrough marks text as strikethrough using Metachat tag.
 func Strikethrough(text string) string {
 	return fmt.Sprintf("#{strikethrough}%s{strikethrough}#", text)
 }
 
+// Preformatted marks text as preformatted using Metachat tag.
 func Preformatted(text string) string {
 	return fmt.Sprintf("#{preformatted}%s{preformatted}#", text)
 }
 
+// Mention marks text as mention using Metachat tag.
 func Mention(text string) string {
 	return fmt.Sprintf("#{mention}%s{mention}#", text)
 }
 
+// Quote marks text as quote using Metachat tag.
 func Quote(text, author string) string {
 	return fmt.Sprintf("#{quote author=%s}%s{quote}#", author, text)
 }
